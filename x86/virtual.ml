cb5c094243c08806ee8fb64eeb9435d90408c274
wataru <wtr816@gmail.com>
2014-10-22 11:50:58 +0900

    edit virtual.ml

--- x86/virtual.ml
+++ x86/virtual.ml
@@ -31,21 +31,22 @@ let expand xts ini addf addi =
     (fun (offset, acc) x t ->
       (offset + 4, addi x t offset acc))
 
-let rec g env = function (* 式の仮想マシンコード生成 (caml2html: virtual_g) *)
+let rec g env (r, e) = (* 式の仮想マシンコード生成 (caml2html: virtual_g) *)
+  match e with
   | Closure.Unit -> Ans(Nop)
   | Closure.Int(i) -> Ans(Set(i))
   | Closure.Float(d) ->
-      let l =
-	try
-	  (* すでに定数テーブルにあったら再利用 *)
-	  let (l, _) = List.find (fun (_, d') -> d = d') !data in
-	  l
-	with Not_found ->
-	  let l = Id.L(Id.genid "l") in
-	  data := (l, d) :: !data;
-	  l in
-      let x = Id.genid "l" in
-      Let((x, Type.Int), SetL(l), Ans(LdDF(x, C(0), 1)))
+     let l =
+       try
+	 (* すでに定数テーブルにあったら再利用 *)
+	 let (l, _) = List.find (fun (_, d') -> d = d') !data in
+	 l
+       with Not_found ->
+	 let l = Id.L(Id.genid "l") in
+	 data := (l, d) :: !data;
+	 l in
+     let x = Id.genid "l" in
+     Let((x, Type.Int), SetL(l), Ans(LdDF(x, C(0), 1)))
   | Closure.Neg(x) -> Ans(Neg(x))
   | Closure.Add(x, y) -> Ans(Add(x, V(y)))
   | Closure.Sub(x, y) -> Ans(Sub(x, V(y)))
@@ -59,35 +60,35 @@ let rec g env = function (* 式の仮想マシンコード生成 (caml2html: virtual_g) *)
   | Closure.FMul(x, y) -> Ans(FMulD(x, y))
   | Closure.FDiv(x, y) -> Ans(FDivD(x, y))
   | Closure.IfEq(x, y, e1, e2) ->
-      (match M.find x env with
+     (match M.find x env with
       | Type.Bool | Type.Int -> Ans(IfEq(x, V(y), g env e1, g env e2))
       | Type.Float -> Ans(IfFEq(x, y, g env e1, g env e2))
       | _ -> failwith "equality supported only for bool, int, and float")
   | Closure.IfLE(x, y, e1, e2) ->
-      (match M.find x env with
+     (match M.find x env with
       | Type.Bool | Type.Int -> Ans(IfLE(x, V(y), g env e1, g env e2))
       | Type.Float -> Ans(IfFLE(x, y, g env e1, g env e2))
       | _ -> failwith "inequality supported only for bool, int, and float")
   | Closure.Let((x, t1), e1, e2) ->
-      let e1' = g env e1 in
-      let e2' = g (M.add x t1 env) e2 in
-      concat e1' (x, t1) e2'
+     let e1' = g env e1 in
+     let e2' = g (M.add x t1 env) e2 in
+     concat e1' (x, t1) e2'
   | Closure.Var(x) ->
-      (match M.find x env with
+     (match M.find x env with
       | Type.Unit -> Ans(Nop)
       | Type.Float -> Ans(FMovD(x))
       | _ -> Ans(Mov(x)))
   | Closure.MakeCls((x, t), { Closure.entry = l; Closure.actual_fv = ys }, e2) -> (* クロージャの生成 (caml2html: virtual_makecls) *)
-      (* Closureのアドレスをセットしてから、自由変数の値をストア *)
-      let e2' = g (M.add x t env) e2 in
-      let offset, store_fv =
-	expand
-	  (List.map (fun y -> (y, M.find y env)) ys)
-	  (4, e2')
-	  (fun y offset store_fv -> seq(StDF(y, x, C(offset), 1), store_fv))
-	  (fun y _ offset store_fv -> seq(St(y, x, C(offset), 1), store_fv)) in
-      Let((x, t), Mov(reg_hp),
-	  Let((reg_hp, Type.Int), Add(reg_hp, C(align offset)),
+     (* Closureのアドレスをセットしてから、自由変数の値をストア *)
+     let e2' = g (M.add x t env) e2 in
+     let offset, store_fv =
+       expand
+	 (List.map (fun y -> (y, M.find y env)) ys)
+	 (4, e2')
+	 (fun y offset store_fv -> seq(StDF(y, x, C(offset), 1), store_fv))
+	 (fun y _ offset store_fv -> seq(St(y, x, C(offset), 1), store_fv)) in
+     Let((x, t), Mov(reg_hp),
+	 Let((reg_hp, Type.Int), Add(reg_hp, C(align offset)),
 	      let z = Id.genid "l" in
 	      Let((z, Type.Int), SetL(l),
 		  seq(St(z, x, C(0), 1),
