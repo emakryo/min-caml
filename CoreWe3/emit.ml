1de3fe681dc5a1c24b5c3fa6109bc832d7168b20
wataru <wtr816@gmail.com>
2014-11-18 04:58:56 +0900

    disable tail call optimization

--- CoreWe3/emit.ml
+++ CoreWe3/emit.ml
@@ -126,7 +126,8 @@ and g' oc = function (* 各命令のアセンブリ生成 *)
      Printf.fprintf oc "\tmtctr\t%s\n\tbctr\n" (reg reg_sw);
   | (Tail, CallDir(Id.L(x), ys)) -> (* 末尾呼び出し *)
      g'_args oc [] ys;
-     Printf.fprintf oc "\tBEQ\tr0\tr0\t:%s\n" x;
+     Printf.fprintf oc "\tJSUB\t:%s\n" x;
+     Printf.fprintf oc "\tRET\n"
   | (NonTail(a), CallCls(x, ys)) ->
      g'_args oc [(x, reg_cl)] ys;
      let ss = stacksize () in
@@ -198,3 +199,4 @@ let f oc (Prog(data, fundefs, e))  =
   stackset := S.empty;
   stackmap := [];
   g oc (NonTail("r0"), e);
+  Printf.fprintf oc "\tRET\n";
