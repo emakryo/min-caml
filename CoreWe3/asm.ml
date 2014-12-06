149d7a83c40f62f44e541eddd98f6746940fd347
wataru <wtr816@gmail.com>
2014-12-06 15:55:32 +0900

    fix immediate optimization

--- CoreWe3/asm.ml
+++ CoreWe3/asm.ml
@@ -111,8 +111,8 @@ let rec concat e1 xt e2 = match e1 with
 (* align : int -> int *)
 let align i = if i mod 8 = 0 then i else i + 4
 
-let imm_max = Int32.of_int 0x4000
-let imm_min = Int32.of_int (-0x4000)
+let imm_max = Int32.of_int 0x2000
+let imm_min = Int32.of_int (-0x2000)
 
 let load_ext_var l =
   let addr = Id.genid (Id.pp_l(l)) in
--- CoreWe3/simm.ml
+++ CoreWe3/simm.ml
@@ -2,19 +2,21 @@ open Asm
 
 let rec g env = function (* 命令列の 16 bit 即値最適化 *)
   | Ans(exp) -> Ans(g' env exp)
-  | Let((x, t), Li(i), e) when ((t = Type. Int) || (t = Type.Bool)) && (imm_min <= i) && (i < imm_max) -> 
+  | Let((x, t), Li(i), e) when (t = Type.Bool || t = Type.Int)  && (imm_min <= i) && (i < imm_max) ->
      let e' = g (M.add x (Int32.to_int i) env) e in
      if List.mem x (fv e') then Let((x, t), Li(i), e') else e'
   | Let(xt, exp, e) -> Let(xt, g' env exp, g env e)
 and g' env = function (* 各命令の 16 bit 即値最適化 *)
-  | Add(x, V(y)) when M.mem y env -> Add(x, C(M.find y env))
-  | Add(x, V(y)) when M.mem x env -> Add(y, C(M.find x env))
-  | Sub(x, y) when M.mem y env -> Add(x, C(-(M.find y env)))
-  | Slw(x, V(y)) when M.mem y env -> Slw(x, C(M.find y env))
-  | Srw(x, V(y)) when M.mem y env -> Slw(x, C(M.find y env))
-  | IfEq(x, y', e1, e2) -> IfEq(x, y', g env e1, g env e2)
-  | IfLE(x, y', e1, e2) -> IfLE(x, y', g env e1, g env e2)
-  | IfFLE(x, y', e1, e2) -> IfFLE(x, y', g env e1, g env e2)
+  | Add(x, V(y)) when M.mem y env ->
+     Add(x, C(M.find y env))
+  | Add(x, V(y)) when M.mem x env ->
+     Add(y, C(M.find x env))
+  | Sub(x, y) when M.mem y env ->
+     Add(x, C(-(M.find y env)))
+  | Slw(x, V(y)) when M.mem y env ->
+     Slw(x, C(M.find y env))
+  | Srw(x, V(y)) when M.mem y env ->
+     Srw(x, C(M.find y env))
   | e -> e
 
 (* トップレベル関数の 16 bit 即値最適化 *)
@@ -23,4 +25,5 @@ let h { name = l; args = xs; body = e; ret = t } =
 
 (* プログラム全体の 16 bit 即値最適化 *)
 let f (Prog(data, fundefs, e)) = 
+  (* Prog(data, fundefs, e) *)
   Prog(data, List.map h fundefs, g M.empty e)
