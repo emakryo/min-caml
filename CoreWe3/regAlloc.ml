eaef4b29dbe1cdb7f4adaf3c5c0efac18e74fcbf
wataru <wtr816@gmail.com>
2015-02-18 18:00:16 +0900

    change implementation of use_set/def_set

--- CoreWe3/regAlloc.ml
+++ CoreWe3/regAlloc.ml
@@ -1,6 +1,7 @@
 open Asm
 
-let use_set = function
+let use_set (_, e, _) = 
+  match e with
   | Nop | Li(_) | FLi(_) | LoadLabel(_) | Restore(_) -> 
      S.empty
   | Ld(_, x, _) | FLd(_, x, _) | IToF(_, x) | FToI(_, x) (* | IAsF(_, x) | FAsI(_, x) *) | Neg(_, x)  | FInv(_, x) | FAbs(_, x) | Sqrt(_, x) | Mr(_, x) | Save(x, _) | FMr(_, x) -> 
@@ -12,15 +13,13 @@ let use_set = function
   | Call (_, _, xs) -> 
      S.of_list xs
 
-let def_set = function
+let def_set (_, e, _) =
+  match e with
   | Nop | St(_) | FSt(_) | If(_) | IfF(_) | Save(_) ->
      S.empty
   | Ld(xt, _, _) | FLd(xt, _, _) | IToF(xt, _) | FToI(xt, _) | Neg(xt, _) | Add(xt, _, _) | Sub(xt, _, _) | And(xt, _, _) | Or(xt, _, _) | Li(xt, _) | Shl(xt, _, _) | Shr(xt, _, _) | FAdd(xt, _, _) | FSub(xt, _, _) | FMul(xt, _, _) | FInv(xt, _) | FAbs(xt, _) | Sqrt(xt, _) | FLi(xt, _) | Call(xt, _, _) | LoadLabel(xt, _) | Mr(xt, _) | FMr(xt, _) | Restore(xt, _) (* | IAsF(xt, _) | FAsI(xt, _) *) ->
      S.singleton (fst xt)
 
 let f (Prog(fundefs, e)) = (* プログラム全体のレジスタ割り当て (caml2html: regalloc_f) *)
+  Format.eprintf "register allocation: may take some time (up to a few minutes, depending on the size of functions)@.";
   Prog(fundefs, e)
-  (* Format.eprintf "register allocation: may take some time (up to a few minutes, depending on the size of functions)@."; *)
-  (* let fundefs' = List.map h fundefs in *)
-  (* let e', regenv' = g (Id.gentmp Type.Unit, Type.Unit) (Ans(Nop)) M.empty e in *)
-  (* Prog(data, fundefs', e') *)
