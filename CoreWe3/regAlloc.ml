4ac9141185728227bbe04d4a3e1f6ff218a816c6
wataru <wtr816@gmail.com>
2015-02-20 04:24:59 +0900

    remove codes for liveness analisys

--- CoreWe3/regAlloc.ml
+++ CoreWe3/regAlloc.ml
@@ -1,40 +1,11 @@
 open Asm
 
-type liveset = S.t * S.t (*入口生存 * 出口生存*)
-
-let use_set (_, e, _) = 
-  match e with
-  | Nop | Li(_) | FLi(_) | LoadLabel(_) | Restore(_) -> 
-     S.empty
-  | Ld(_, x, _) | FLd(_, x, _) | IToF(_, x) | FToI(_, x) (* | IAsF(_, x) | FAsI(_, x) *) | Neg(_, x)  | FInv(_, x) | FAbs(_, x) | Sqrt(_, x) | Mr(_, x) | Save(x, _) | FMr(_, x) -> 
-     S.singleton x
-  | St(x, y, _) | FSt(x, y, _) | Sub(_, x, y) | And(_, x, y) | Or(_, x, y) | FAdd(_, x, y) | FSub(_, x, y) | FMul(_, x, y) | IfF(_, (x, y), _, _) -> 
-     S.of_list [x; y]
-  | Add(_, x, y') | Shl(_, x, y')| Shr(_, x, y') | If(_, (x, y'), _, _) -> 
-     S.of_list (x::fv_id_or_imm y')
-  | Call (_, _, xs) -> 
-     S.of_list xs
-
-let def_set (_, e, _) =
-  match e with
-  | Nop | St(_) | FSt(_) | If(_) | IfF(_) | Save(_) ->
-     S.empty
-  | Ld(xt, _, _) | FLd(xt, _, _) | IToF(xt, _) | FToI(xt, _) | Neg(xt, _) | Add(xt, _, _) | Sub(xt, _, _) | And(xt, _, _) | Or(xt, _, _) | Li(xt, _) | Shl(xt, _, _) | Shr(xt, _, _) | FAdd(xt, _, _) | FSub(xt, _, _) | FMul(xt, _, _) | FInv(xt, _) | FAbs(xt, _) | Sqrt(xt, _) | FLi(xt, _) | Call(xt, _, _) | LoadLabel(xt, _) | Mr(xt, _) | FMr(xt, _) | Restore(xt, _) (* | IAsF(xt, _) | FAsI(xt, _) *) ->
-     S.singleton (fst xt)
-
-let rec init_live mp = function
-  | [] -> mp
-  | e::es -> init_live' (init_live mp es) e
-and init_live' mp (i, e, b) = 
-  let mp' = 
-    match e with
-    | If(_, _, e_then, e_else) 
-    | IfF(_, _, e_then, e_else)-> 
-       let mp  = init_live mp e_then in
-       init_live mp e_else
-    | _ -> mp in
-  AM.add (i, e, b) (S.empty, S.empty) mp' 
+let h ({ name = Id.L(x); args = _; body = e; ret = t } as fdef) =
+  let s = Liveness.h fdef in
+  ()
 
 let f (Prog(fundefs, e)) = (* プログラム全体のレジスタ割り当て (caml2html: regalloc_f) *)
   Format.eprintf "register allocation: may take some time (up to a few minutes, depending on the size of functions)@.";
+  List.iter h fundefs;  
+  let s = Liveness.g e in  
   Prog(fundefs, e)
